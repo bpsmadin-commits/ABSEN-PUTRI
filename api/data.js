30e2bc7f70f95efd937a3096d6a5b7b5a6849632
bpsmadin-commits <bpsmadin@gmail.com>
2026-09-01 02:13:16 +0700

    Refactor data handler to filter absensi by date

--- api/data.js
+++ api/data.js
@@ -1,5 +1,6 @@
 export default async function handler(req, res) {
   const DB_URL = "https://absenputri-5d8a1-default-rtdb.asia-southeast1.firebasedatabase.app";
+  const { tanggal } = req.query;
 
   try {
     const [absensiRes, jadwalRes] = await Promise.all([
@@ -7,15 +8,24 @@ export default async function handler(req, res) {
       fetch(`${DB_URL}/jadwal.json`)
     ]);
 
-    const absensi = await absensiRes.json();
+    let absensi = await absensiRes.json();
     const jadwal = await jadwalRes.json();
+
+    if (tanggal) {
+      const hasil = {};
+      for (const lembaga in absensi) {
+        if (absensi[lembaga][tanggal]) {
+          hasil[lembaga] = { [tanggal]: absensi[lembaga][tanggal] };
+        }
+      }
+      absensi = hasil;
+    }
 
     res.setHeader("Content-Type", "text/plain; charset=utf-8");
     res.status(200).send(
-      "=== DATA ABSENSI ===\n" +
+      "=== DATA ABSENSI" + (tanggal ? ` (${tanggal})` : "") + " ===\n" +
       JSON.stringify(absensi, null, 2) +
-      "\n\n=== DATA JADWAL ===\n" +
-      JSON.stringify(jadwal, null, 2)
+      (tanggal ? "" : "\n\n=== DATA JADWAL ===\n" + JSON.stringify(jadwal, null, 2))
     );
   } catch (err) {
     res.status(500).send("Gagal ambil data: " + err.message);
